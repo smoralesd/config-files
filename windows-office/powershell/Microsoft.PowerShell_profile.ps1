07dd693a4db5ec17189f5b004839c6d17d0e141d
Sebastian Morales <semora@microsoft.com>
2017-05-05 17:00:18 -0700

    Add powershell profile

---
+++ windows-office/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+﻿New-Alias g git
+
+Import-Module 'C:\Users\semora\Repos\posh-git\src\posh-git.psd1'
+
+$GitPromptSettings.BeforeText = "("
+$GitPromptSettings.AfterText = ")"
+$GitPromptSettings.BranchIdenticalStatusToSymbol = ""
