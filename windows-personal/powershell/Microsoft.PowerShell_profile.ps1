db9dac4dbd96c508c88af5d4f9b3724a220087dd
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-19 11:41:37 -0700

    Add powershell profile

---
+++ windows-personal/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+﻿New-Alias g git
+
+# Install posh-git using Import-Module for the next line to actually work
+Import-Module posh-git
+
+$GitPromptSettings.BeforeText = "("
+$GitPromptSettings.AfterText = ")"
+$GitPromptSettings.BranchIdenticalStatusToSymbol = ""
