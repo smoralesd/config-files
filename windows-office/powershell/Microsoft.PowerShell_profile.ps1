3219a59a0a67bb6eac5642c8187ff416a08b4440
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-08-30 16:12:31 -0700

    update powershell config for PS 6

--- windows-office/powershell/Microsoft.PowerShell_profile.ps1
+++ windows-office/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,9 +4,12 @@ New-Alias y yarn
 # Install posh-git using Import-Module for the next line to actually work
 Import-Module posh-git
 
-$GitPromptSettings.BeforeText = "("
-$GitPromptSettings.AfterText = ")"
-$GitPromptSettings.BranchIdenticalStatusToSymbol = ""
+$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+
+$GitPromptSettings.BeforeStatus = "("
+$GitPromptSettings.AfterStatus = ")"
+$GitPromptSettings.BranchIdenticalStatusSymbol = ""
+
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
