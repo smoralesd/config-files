2710626950694083f364f2f72bbae050f9156085
Sebastian Morales <sebastian.moralesd@gmail.com>
2020-04-14 15:04:02 -0700

    customize powershell prompt

--- windows-office/powershell/Microsoft.PowerShell_profile.ps1
+++ windows-office/powershell/Microsoft.PowerShell_profile.ps1
@@ -9,9 +9,66 @@ $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
 $GitPromptSettings.BeforeStatus = "("
 $GitPromptSettings.AfterStatus = ")"
 $GitPromptSettings.BranchIdenticalStatusSymbol = ""
+$GitPromptSettings.DefaultPromptPath.ForegroundColor = 'Orange'
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
+}
+
+function prompt {
+    # $Success = $?
+
+    # $prompt += & $GitPromptScriptBlock
+    $Success = $?
+
+    ## Time calculation
+    $LastExecutionTimeSpan = if (@(Get-History).Count -gt 0) {
+        Get-History | Select-Object -Last 1 | ForEach-Object {
+            New-TimeSpan -Start $_.StartExecutionTime -End $_.EndExecutionTime
+        }
+    }
+    else {
+        New-TimeSpan
+    }
+
+    $LastExecutionShortTime = if ($LastExecutionTimeSpan.Days -gt 0) {
+        "$($LastExecutionTimeSpan.Days + [Math]::Round($LastExecutionTimeSpan.Hours / 24, 2)) d"
+    }
+    elseif ($LastExecutionTimeSpan.Hours -gt 0) {
+        "$($LastExecutionTimeSpan.Hours + [Math]::Round($LastExecutionTimeSpan.Minutes / 60, 2)) h"
+    }
+    elseif ($LastExecutionTimeSpan.Minutes -gt 0) {
+        "$($LastExecutionTimeSpan.Minutes + [Math]::Round($LastExecutionTimeSpan.Seconds / 60, 2)) m"
+    }
+    elseif ($LastExecutionTimeSpan.Seconds -gt 0) {
+        "$($LastExecutionTimeSpan.Seconds + [Math]::Round($LastExecutionTimeSpan.Milliseconds / 1000, 2)) s"
+    }
+    elseif ($LastExecutionTimeSpan.Milliseconds -gt 0) {
+        "$([Math]::Round($LastExecutionTimeSpan.TotalMilliseconds, 2)) ms"
+    }
+    else {
+        "0 s"
+    }
+
+    if ($Success) {
+        Write-Host -Object "[$LastExecutionShortTime] " -NoNewline -ForegroundColor Green
+    }
+    else {
+        Write-Host -Object "! [$LastExecutionShortTime] " -NoNewline -ForegroundColor Red
+    }
+
+    ## History ID
+    $HistoryId = $MyInvocation.HistoryId
+    # Uncomment below for leading zeros
+    $HistoryId = '{0:d3}' -f $MyInvocation.HistoryId
+    Write-Host -Object "$HistoryId`: " -NoNewline -ForegroundColor Cyan
+
+    ## User
+    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    Write-Host -Object "$($env:USERNAME) ($(if ($IsAdmin){ 'A' } else { 'U' })) " -NoNewline -ForegroundColor DarkRed
+
+    ## return git status
+    return & $GitPromptScriptBlock
 }
